237225ae23ef39420e3b801b5cc75f600a68c995
Brendan Murty <brendan@brendanmurty.com>
2014-09-06 09:19:11 +1000

    Improve display of link detail page. Resolves #1.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,7 @@
 <?
 
 // Site settings and general variables
-$css_update_date = "20140905c";
+$css_update_date = "20140906a";
 $js_update_date = "20140823f";
 $nl = "\r\n";
 $page_type = "";
--- site/templates/link.php
+++ site/templates/link.php
@@ -5,7 +5,7 @@ $link_tags = post_tags($page,'inline');
 ?>
 <article class="item item-link">
 	<ul class="about">
-		<li class="date">Saved <?= $link_date ?><span class="separator">in</span></li>
+		<li class="date">Saved <?= $link_date ?></li>
 		<li class="tags"><?= $link_tags ?></li>
 	</ul>
 	<?php echo markdown($page->text()) ?>
