5861a45ff97d3b78efc34f5eccb9e60365269382
Brendan Murty <brendan@brendanmurty.com>
2015-04-24 06:49:22 +1000

    Remove old work history content and resumé logic

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,11 +2,7 @@
 <article>
 <?
 echo markdown($page->text());
-if($site->uri()->path()->first()=='resume'){
-    // Resumé page
-    echo '<a class="email" href="mailto:?subject=Resumé - Brendan Murty&body=Link to resumé: http://brendanmurty.com/resume" title="Email a link to this resumé">Email</a>';
-    echo '<a class="print" href="javascript:window.print();" title="Print this resumé">Print</a>';
-}elseif($site->uri()->path()=='tags'){
+if($site->uri()->path()=='tags'){
     // All tags list
     $items=$pages->find('link/','post/');
     $tags=tagcloud($items,array('field'=>'tags','sort'=>'name','sortdir'=>'asc'));
