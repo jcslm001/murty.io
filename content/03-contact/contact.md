e9285313f31bb14afa68ee65a8258985fd362744
Brendan Murty <brendan@murty.id.au>
2016-01-16 12:24:30 +1100

    Hide resume page, remove contact page

--- content/03-contact/contact.md
+++
@@ -1,9 +0,0 @@
-Title: Contact
-----
-Text:
-
-I have extensive commercial experience working on many areas of websites and web platforms with hundreds of users.
-
-I have a competitive hourly rate and I'm happy to sign a non-disclosure agreement if your project requires it.
-
-Let's have a chat.
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,23 +1,22 @@
 		</section>
-		<?
-
+		<?php
 		// Configure the item actions elements
 		$page_name = $page->uri();
 		$nav_actions = '';
-		if($page_name == 'link'){
-			$nav_actions  = '<a class="visit-link" href="'.$page->link().'">Visit link</a>';
-			$nav_actions .= '<a class="twitter" href="http://twitter.com/share?url='.$page->url().'&amp;text=Link%3A+'.urlencode($page->title()).'">Share on Twitter</a></li>';
-			$nav_actions .= '<a class="facebook" href="http://www.facebook.com/sharer.php?u='.$page->url().'&amp;t=Link%3A+'.urlencode($page->title()).'">Post to Facebook</a>';
-		}elseif($page_name == 'post'){
+
+		if($page_name == 'post'){
+			// Individual post page
 			$nav_actions  = '<a class="twitter" href="http://twitter.com/share?url='.$page->url().'&amp;text='.urlencode($page->title()).'+by+%40brendanmurty">Share on Twitter</a>';
 			$nav_actions .= '<a class="facebook" href="http://www.facebook.com/sharer.php?u='.$page->url().'&amp;t='.urlencode($page->title()).'+by+Brendan+Murty">Post to Facebook</a></li>';
 		}elseif($page_name == 'about'){
-			$nav_actions  = '<a href="/contact">Contact me</a>';
-			$nav_actions .= '<a href="/resume">View my resumé</a>';
-		}elseif($page_name == 'contact'){
+			// About page
 			$nav_actions  = '<a href="mailto:brendan@murty.id.au">Email me</a>';
+			$nav_actions .= '<a href="/resume">View my resumé</a>';
 		}
-		if($nav_actions!='') echo '<nav class="actions">'.$nav_actions.'</nav>';
+
+		if($nav_actions != '') {
+			echo '<nav class="actions">' . $nav_actions . '</nav>';
+		}
 
 		if ($page_name != 'resume') {
 		?>
