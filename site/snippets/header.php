3e2c810a2ee67ddc1f103c37536685fa14b6762b
Brendan Murty <b@murty.io>
2016-06-10 22:13:40 +1000

    Use a smaller profile image

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -104,7 +104,7 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
         <?php if ($page_type == 'home' || $page_name == 'resume') { ?>
         <header>
             <a class="profile" href="/about" title="Learn more about me">
-                <img src="/assets/images/common/brendan-murty.jpg" height="200" width="200" alt="Profile picture of Brendan" />
+                <img src="/assets/images/common/brendan_murty.jpg" height="200" width="200" alt="Profile picture of Brendan" />
             </a>
 
             <?php if ($page_type == 'home') { ?>
