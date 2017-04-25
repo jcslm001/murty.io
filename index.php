b3c93baf952df6f6831b44ae63e2d781e7fe312d
Brendan Murty <b@murty.io>
2017-04-25 10:59:00 +1000

    Simplify redirects (#22)

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php include('lib/redirects.php'); ?><!doctype html>
+<?php
+
+// Redirection for older pages
+
+if ($_SERVER['HTTP_HOST'] == 'b.murty.io') {
+    $url = $_SERVER['REQUEST_URI'];
+
+    if ($_SERVER['REQUEST_URI'] == '/post/farewell-upcomingtasks') {
+        $url = '/post/20161014_farewell-upcomingtasks';
+    }
+
+    header('Location: http://murty.io/brendan' . $url);
+} elseif ($_SERVER['HTTP_HOST'] == 'i.murty.io') {
+    header('Location: http://murty.io/isla' . $_SERVER['REQUEST_URI']);
+}
+
+?><!doctype html>
 <html lang="en" ng-app="murtyApp" ng-controller="headerCtrl">
   <head>
     <meta charset="utf-8">
