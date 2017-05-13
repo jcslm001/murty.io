d962eb2027cfe031349c7cd6687c3be90fbf0ff1
Brendan Murty <b@murty.io>
2017-05-12 23:18:54 -0400

    Improved shortcut subdomain redirection

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<?php
-
-// Redirection for older pages
-
-if ($_SERVER['HTTP_HOST'] == 'b.murty.io') {
-    $url = $_SERVER['REQUEST_URI'];
-
-    if ($_SERVER['REQUEST_URI'] == '/post/farewell-upcomingtasks') {
-        $url = '/post/20161014_farewell-upcomingtasks';
-    }
-
-    header('Location: http://murty.io/brendan' . $url);
-} elseif ($_SERVER['HTTP_HOST'] == 'i.murty.io') {
-    header('Location: http://murty.io/isla' . $_SERVER['REQUEST_URI']);
-}
-
-?><!doctype html>
+<!doctype html>
 <html lang="en" ng-app="murtyApp" ng-controller="headerCtrl">
   <head>
     <meta charset="utf-8">
