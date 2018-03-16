069f3e87bd2c0d3841aac5dfbc03d1a09767a051
Brendan Murty <b@murty.io>
2018-03-17 10:03:39 +1100

    General: Fix "sites.json" redirect

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ router.get('/api/sites', sites_controller.get);
 // Custom redirects
 router.get('/readme.md',            function(request, response) { response.redirect('https://bitbucket.org/brendanmurty/murty.io/src/master/readme.md'); });
 router.get('/license.md',           function(request, response) { response.redirect('https://bitbucket.org/brendanmurty/murty.io/src/master/license.md'); });
-router.get('/sites.json',           function(request, response) { response.sendFile('https://bitbucket.org/brendanmurty/murty.io/src/master/sites.json'); });
+router.get('/sites.json',           function(request, response) { response.redirect('https://bitbucket.org/brendanmurty/murty.io/src/master/sites.json'); });
 router.get('/code',                 function(request, response) { response.redirect('https://bitbucket.org/brendanmurty/murty.io'); });
 router.get('/brendan/posts.json',   function(request, response) { response.sendFile(__dirname + '/content/brendan/posts/feed.json'); });
 
