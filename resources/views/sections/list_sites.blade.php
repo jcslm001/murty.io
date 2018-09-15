017b7bca0dbdea3a853ef19bfd1bfb2263eaff29
Brendan Murty <b@murty.io>
2018-09-15 22:05:51 +1000

    Update main sites listing to ensure the right domain is used

--- resources/views/sections/list_sites.blade.php
+++ resources/views/sections/list_sites.blade.php
@@ -1,6 +1,6 @@
 <ul class="sites">
     <li>
-        <a href="/brendan">
+        <a href="https://murty.io/brendan">
             <img src="/images/brendan/brendan_murty.jpg" alt="BCM" title="Visit Brendan Murty's website">
             <span>Brendan Murty</span>
         </a>
@@ -12,13 +12,13 @@
         </a>
     </li>
     <li>
-        <a href="/isla">
+        <a href="https://murty.io/isla">
             <img src="/images/isla/isla_murty.jpg" alt="IJM" title="Visit Isla Murty's website">
             <span>Isla Murty</span>
         </a>
     </li>
     <li>
-        <a href="/freya">
+        <a href="https://murty.io/freya">
             <img src="/images/freya/freya_murty.jpg" alt="FJM" title="Visit Freya Murty's website">
             <span>Freya Murty</span>
         </a>
