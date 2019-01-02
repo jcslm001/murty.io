5565796e0ce690fd6d62e5643bf791c231710bcc
Brendan Murty <b@murty.io>
2019-01-02 13:46:55 +1100

    Core: Remove Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,6 @@
         @if (!empty($site['microblog_url']))
         <link rel="me" href="{{ $site['microblog_url'] }}">
         @endif
-
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-97381586-1', 'auto');
-            ga('send', 'pageview');
-        </script>
     </head>
     <body @if(!empty($site['body_class']))class="{{ $site['body_class'] }}"@endif>
         <section id="sidebar">
